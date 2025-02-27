b118165a26d9db17b3481d2afb9fe9a2084216e1
Roman Pogrebnyak <grabberisonfire@gmail.com>
2025-02-27 19:34:14 +0300

    rasterization

---
+++ CMakeFiles/DirectX12.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ropero/code/CGGD/src/renderer/dx12/dx12_renderer.cpp" "CMakeFiles/DirectX12.dir/src/renderer/dx12/dx12_renderer.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/renderer/dx12/dx12_renderer.cpp.o.d"
+  "/home/ropero/code/CGGD/src/renderer/renderer.cpp" "CMakeFiles/DirectX12.dir/src/renderer/renderer.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/renderer/renderer.cpp.o.d"
+  "/home/ropero/code/CGGD/src/settings.cpp" "CMakeFiles/DirectX12.dir/src/settings.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/settings.cpp.o.d"
+  "/home/ropero/code/CGGD/src/utils/resource_utils.cpp" "CMakeFiles/DirectX12.dir/src/utils/resource_utils.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/utils/resource_utils.cpp.o.d"
+  "/home/ropero/code/CGGD/src/utils/window.cpp" "CMakeFiles/DirectX12.dir/src/utils/window.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/utils/window.cpp.o.d"
+  "/home/ropero/code/CGGD/src/win_main.cpp" "CMakeFiles/DirectX12.dir/src/win_main.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/win_main.cpp.o.d"
+  "/home/ropero/code/CGGD/src/world/camera.cpp" "CMakeFiles/DirectX12.dir/src/world/camera.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/world/camera.cpp.o.d"
+  "/home/ropero/code/CGGD/src/world/model.cpp" "CMakeFiles/DirectX12.dir/src/world/model.cpp.o" "gcc" "CMakeFiles/DirectX12.dir/src/world/model.cpp.o.d"
+  "" "DirectX12" "gcc" "CMakeFiles/DirectX12.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
